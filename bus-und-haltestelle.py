037bb9800aac206830090adcd07f611a2e28f088
usernamehanillcoer <171wita08@o365.htl-leoben.at>
2018-04-09 14:42:12 +0200

    alles okey

---
+++ bus-und-haltestelle.py
@@ -0,0 +1,6 @@
+eingabe = input("Anzahl der Haltestellen eingeben")
+eingabe = int(eingabe)
+for i in range(1 , eingabe):
+  anzahl = input("Wie viele personen steigen ein?")
+  summe = anzahl + anzahl
+print("Es sind: ", summe , " personen im bus")
